f5e2497a3f1164e3763c36ea0ba7ae2d3040060e
PatNowak <patryk_nowak@yahoo.pl>
2016-08-06 13:47:17 +0200

    added nats

--- lib/RandomUserApi/engine.ex
+++ lib/RandomUserApi/engine.ex
@@ -4,15 +4,23 @@ defmodule RandomUserApi.Engine do
   @random_me_url "http://api.randomuser.me"
 
   @doc """
-  Gets n number of random users.
+  Function that generates n number of random users.
   Return list of maps with the users.
+
+  ## Parameters
+  - number: integer that represents number of of required random users.
+  - gender: atom, :male or :female. If something different is passed, it won't be applied.
+  - nat: list of nationalities, comma-separated. In handles only known nationalities by the API.
   """
 
-  def get_users(number, gender) do
+  def get_users(number, gender, nat \\ []) do
     url = @random_me_url <> "?results=#{number}"
-    if _validate_gender gender do
-      url = url <> "&gender=#{gender}"
-    end
+    url = if _validate_gender(gender), do: url <> "&gender=#{gender}", else: url
+    nat = _validate_nat(nat)
+    url = if nat != "", do: url <> "&nat=#{nat}", else: url
+    url
+    |> _fetch_url
+    |> _process
   end
 
   defp _validate_gender(gender) do
@@ -23,10 +31,16 @@ defmodule RandomUserApi.Engine do
     end
   end
 
- # TODO
-  # defp _validate_nat(nat) do
-  #    nats = ~w(AU BR CA CH DE DK ES FI FR GB IE IR NL NZ TR US)
-  # end
+  defp _validate_nat(nat) when is_list(nat) do
+     nats = ~w(AU BR CA CH DE DK ES FI FR GB IE IR NL NZ TR US)
+     nat
+     |> Enum.filter(&(&1 in nats))
+     |> Enum.join(",")
+  end
+
+  defp _validate_nat(_nat) do
+    ""
+  end
 
   defp _process(input) do
     input
@@ -35,8 +49,8 @@ defmodule RandomUserApi.Engine do
   end
 
   defp _fetch_url(url) do
-    IO.puts url
     result = @random_me_api.get url
+    IO.puts inspect result
     case result do
       %{body: data} -> data
       %{message: _} -> :error
